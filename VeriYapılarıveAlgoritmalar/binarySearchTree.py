4e204e78be0dad1075c64665cbf01edf573f01c2
Ömer <44753206+OmerCelikel@users.noreply.github.com>
2022-04-15 21:10:20 +0300

    Create binarySearchTree.py

---
+++ VeriYapılarıveAlgoritmalar/binarySearchTree.py
@@ -0,0 +1,11 @@
+"""
+1)array'i sortla
+2) Dizinin Ortasını alın ve kök yapın.
+3) Sol yarı ve sağ yarı için yinelemeli olarak aynısını yapın.
+       a) Sol yarının ortasını alın ve kökün sol çocuğu yapın
+           1. adımda oluşturuldu.
+       b) Sağ yarının ortasını alın ve onu sağın çocuğu yapın.
+           1. adımda oluşturulan kök.
+           
+"""
+
